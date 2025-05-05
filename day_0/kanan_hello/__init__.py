6ead9cdcb6d51fade687d0569797e2c50bbef2c2
kanangulati21 <kanan.gulati@acceron.in>
2025-05-06 04:06:57 +0530

    exercise-2 completed install 'rich' package and enhance output with rich formatting

--- day_0/kanan_hello/__init__.py
+++ day_0/kanan_hello/__init__.py
@@ -1,3 +1,7 @@
-from .main import say_hello
+# from .main import say_hello
 
-__all__ = ["say_hello"]
+# __all__ = ["say_hello"]
+
+
+from .main import say_hello, print_rich_hello
+__all__ = ["say_hello", "print_rich_hello"]
